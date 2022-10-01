482c0d1851d8e31c507dd184e27509c371bde77f
muregii <88530358+muregii@users.noreply.github.com>
2022-09-30 20:58:06 -0400

    Create maths.java

---
+++ maths.java
@@ -0,0 +1,19 @@
+package com.raydonmuregi.demo;
+
+public class Main {
+  public static void main (String [] args) {
+    //Augmented assignment operators
+    int x = 2;
+    x+= 1;
+    System.out.println(x);
+  }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        //implicit casting
+        short x = 4;
+       int y= x += x;
+        System.out.println(y);
+    }
+}
