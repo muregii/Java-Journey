7d0eb69a627dd213dfc27a33449451fcee340592
muregii <88530358+muregii@users.noreply.github.com>
2022-09-30 21:10:37 -0400

    Update maths.java

--- maths.java
+++ maths.java
@@ -1,3 +1,5 @@
+//Concept 1
+
 package com.raydonmuregi.demo;
 
 public class Main {
@@ -9,6 +11,9 @@ public class Main {
   }
 }
 
+//Concept 2
+
+
 public class Main {
     public static void main(String[] args) {
         //implicit casting
@@ -17,3 +22,18 @@ public class Main {
         System.out.println(y);
     }
 }
+
+
+//Concept 3
+
+public class Main {
+    public static void main(String[] args) {
+        //implicit casting - happens when there's no chance for data loss
+        //byte > short > int > long > double
+        double x = 4.1;
+       int y = (int) x + 2; //2.0 /* explicit casting of x. Must be compatible, meaning if x was String, you could not cast x to an integer*/
+        // to convert a string to an integer, we use something called "parsing"
+        String z = "4";
+        int c = Integer.parseInt(z) + 3;
+        System.out.println(z);
+    }
