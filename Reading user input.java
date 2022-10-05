5bac9e7da7de8a5871277ed8ee6bce540f372fd3
muregii <88530358+muregii@users.noreply.github.com>
2022-10-05 00:31:03 -0400

    Update Reading user input.java

--- Reading user input.java
+++ Reading user input.java
@@ -15,6 +15,25 @@ public class Main {
     }
 }
 
+/*******************************************************************************************************************************/
+
 //Reading the names of a person
 
+package com.raydonmuregi.demo;
 
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Reading input from the user
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name: ");
+        String name = scanner.nextLine().trim(); // This cannot print two names because names are "tokens, "every time we call the .next method it reads only one token. We therefore use .nextLine You can use the tri method to cut extra white spaces that may be at the start of the input. This is again a use of Chaining methods
+        System.out.println("You are " + name); //concatenating a string and a boolean. Implicit conversion/casting will happen.
+    }
+}
+
+
+/*******************************************************************************************************************************/
