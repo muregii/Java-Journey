98b12ce483af5d5eb62f91ffcd27f405040d152c
muregii <88530358+muregii@users.noreply.github.com>
2022-10-06 23:32:39 -0400

    Update mortgage calculator.java

--- mortgage calculator.java
+++ mortgage calculator.java
@@ -1,4 +1,4 @@
-/* Create a morgage calculator that prompts the user to enter the amount of loan they want to get.
+/* Create a mortgage calculator that prompts the user to enter the amount of loan they want to get.
 Predetermine the annual interest rate, and the payback period of the loan in years. 
 The calculator should then return the monthly instalments required based on inputs entered. 
 Use the formula M = P (1+r)^n / (1+r)^n -1
