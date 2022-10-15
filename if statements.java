161016578be3f0640a4b934cdfa2c2beca58d72c
muregii <88530358+muregii@users.noreply.github.com>
2022-10-14 23:16:33 -0400

    Create if statements.java

---
+++ if statements.java
@@ -0,0 +1,17 @@
+package com.raydonmuregi.demo;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        int temperature = 16;
+        if(temperature >50) {
+            System.out.println("It's hot outside");
+        }else if (temperature >70)
+            System.out.println("It's a beautiful day Raydon");
+         else
+            System.out.println("Cold day");
+    }
+
+}
