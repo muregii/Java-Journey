d3feb6b3a3453379602784abed27e8025fd7efa0
muregii <88530358+muregii@users.noreply.github.com>
2022-10-14 23:36:21 -0400

    Update code types-Amateur-Intermediate-Professional.java

--- code types-Amateur-Intermediate-Professional.java
+++ code types-Amateur-Intermediate-Professional.java
@@ -12,7 +12,7 @@ public class Main{
             hasHighIncome = true;
         else
             hasHighIncome = false;
-
+        System.out.println(hasHighIncome);
     }
 
 }
@@ -29,7 +29,26 @@ public class Main{
         boolean hasHighIncome = false;
         if (income > 100_000)
             hasHighIncome = true;
-
+        System.out.println(hasHighIncome);
     }
 
 }
+
+package com.raydonmuregi.demo;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        //Professional code
+        int income = 120_000;
+        boolean hasHighIncome = income > 100_000;
+       
+        System.out.println(hasHighIncome);
+    }
+
+}
+
+
+
