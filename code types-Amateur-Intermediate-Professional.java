5033fd5725cab7d44697db01d04a034c2ee4b59b
muregii <88530358+muregii@users.noreply.github.com>
2022-10-14 23:21:19 -0400

    Create code types-Amateur-Intermediate-Professional.java

---
+++ code types-Amateur-Intermediate-Professional.java
@@ -0,0 +1,18 @@
+package com.raydonmuregi.demo;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        //Amateurish code
+        int income = 120_000;
+        boolean hasHighIncome;
+        if (income > 100_000)
+            hasHighIncome = true;
+        else
+            hasHighIncome = false;
+
+    }
+
+}
