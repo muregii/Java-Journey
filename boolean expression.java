fb35900bf9ed85dc063386bdf72100c76c25e90a
muregii <88530358+muregii@users.noreply.github.com>
2022-10-14 22:27:15 -0400

    Create boolean expression.java

---
+++ boolean expression.java
@@ -0,0 +1,14 @@
+package com.raydonmuregi.demo;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+      int x = 1;
+      int y = 1;
+      System.out.println( x <= y);
+      System.out.println(x == y);
+    }
+
+}
