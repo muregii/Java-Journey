06f395b1b943a8ae7cfd9638315b0ea0b2d4363c
muregii <88530358+muregii@users.noreply.github.com>
2022-10-14 22:32:21 -0400

    Update boolean expression.java

--- boolean expression.java
+++ boolean expression.java
@@ -3,12 +3,27 @@ package com.raydonmuregi.demo;
 import java.text.NumberFormat;
 import java.util.Scanner;
 
-public class Main{
+public class Main{ 
     public static void main(String[] args){
       int x = 1;
       int y = 1;
-      System.out.println( x <= y);
-      System.out.println(x == y);
+      System.out.println( x <= y); //less than or equal to, return true, else return false
+      System.out.println(x == y); // equal to
     }
 
+}
+
+package com.raydonmuregi.demo;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+      int temperature = 72;
+      boolean isWarm = temperature > 70 && temperature < 75; // logical AND operator
+      System.out.println(isWarm);
+
+    }
+    
 }
