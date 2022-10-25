c7d647e528940452eb5d62efb0f21e78adea7a68
muregii <88530358+muregii@users.noreply.github.com>
2022-10-25 06:28:01 -0400

    Create While loops.java

---
+++ While loops.java
@@ -0,0 +1,19 @@
+package com.raydonmuregi.demo;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        //while loops
+        for (int i = 5; i >0; i--)
+            System.out.println("Hey Raydon" + i);
+
+        int i = 0;
+        while(i>0) {
+            System.out.println("Hey Raydon" + i);
+            i--;
+        }
+        }
+    }
+
