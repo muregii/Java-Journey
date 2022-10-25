725f4aa607652d7231357868890e25722ee33603
muregii <88530358+muregii@users.noreply.github.com>
2022-10-25 06:22:06 -0400

    Create for loops.java

---
+++ for loops.java
@@ -0,0 +1,18 @@
+/* for (int i = 0; i < 5; i++ )
+            System.out.println("Hello Raydon?" + i);*/
+
+
+package com.raydonmuregi.demo;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        // For Loops
+        /*initializing i to zero* /*Java evaluates this condition. Then first loop is executed*/ /*We're in the second loop now*/
+        for (int i = 5; i > 0; i--  )
+            System.out.println("Hello Raydon?" + i);
+        }
+    }
+
