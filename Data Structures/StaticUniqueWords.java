a4c66112e7ff346b30b2fe06e4d5545ce98c6ca6
muregii <88530358+muregii@users.noreply.github.com>
2022-11-27 21:40:59 -0500

    Add files via upload
    This program reads through the entire oxford dictionary, and counts all the words. It then adds all unique words into a set.

---
+++ Data Structures/StaticUniqueWords.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class StaticUniqueWords {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner s = new Scanner(new File("Desktop/DSA code/Oxford English Dictionary.txt"));
+        HashSet<String> set = new HashSet<>();
+        int wcount = 0;
+        double start = System.nanoTime();
+
+        while (s.hasNext()) {
+            wcount +=1;
+            String word = s.next();
+
+            if(! set.contains(word)) {
+                set.add(word);
+            }
+        }
+        double end = System.nanoTime();
+        System.out.printf("total number of words: %d, unique #: %d\n", wcount, set.size());
+        System.out.printf("time taken: %2.3g\n", (end-start)/1e9);
+        s.close();
+
+
+    }
+}
