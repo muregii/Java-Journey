bcb8ca977f3beb9070ef18f70dd23210ee95dcc3
muregii <88530358+muregii@users.noreply.github.com>
2022-12-30 00:08:53 -0500

    Update HashSet-Implementation.java

--- Data Structures/HashSet-Implementation.java
+++ Data Structures/HashSet-Implementation.java
@@ -1,8 +1,18 @@
 public static int countWordHashSet (String[] words) {
 HashSet<String> mySet = new HashSet<>();
-  for(String w: words) {
+  for(String w: words) { //run time complexity "Linear Complexity"
   mySet.add(words);
   }
   return mySet.size();
 }
-//run time complexity "Linear Complexity"
+
+
+public static int countWordHashSet (String[] words) {
+ArrayList<String> myList = new ArrayList<>();
+  for(String w: words) {
+    if(!myList.contains(w)){ //run time complexity "Quadratic Complexity"
+  mySet.add(words);
+    }
+  }
+  return mySet.size();
+}
