59fd31333beb029c4db660963b56a3b008143e81
muregii <88530358+muregii@users.noreply.github.com>
2023-01-12 06:09:14 -0500

    Update Linked-List-Recursion.java

--- Data Structures/Linked-List-Recursion.java
+++ Data Structures/Linked-List-Recursion.java
@@ -1,3 +1,10 @@
+/*copying via recursion
+
+public ListNode copyRec(ListNode list) {
+if(list == null) return null;
+return new ListNode(list.info, copyRec(list.next);
+}
+*/
 public ListNode copy(ListNode list) {
 if(list == null) return null;
   ListNode first = new ListNode(list.info, null);
