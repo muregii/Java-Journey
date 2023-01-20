b4a4b3e24646c67ce49d901e99df780656a661db
muregii <88530358+muregii@users.noreply.github.com>
2023-01-20 16:35:35 -0500

    Create CirclesCountry.java

---
+++ Data Structures/APTS/CirclesCountry.java
@@ -0,0 +1,25 @@
+/*Circles Country is a country that contains several circular-shaped districts. 
+Some districts may be situated inside other districts, but their borders do not intersect or touch. Qatam is a resident of Circles Country. When he travels between two locations, 
+he always tries to cross the fewest number of district borders as possible because crossing borders is usually a laborious task.
+Imagine Circles Country as an infinite plane. You are given int[] x and int[] y and int[] r, where (x[i],y[i]) are the coordinates of the i-th district's center and r[i] is its radius. Qatam is currently at point (x1,y1) and he needs to get to point (x2,y2). 
+Neither of these points lies on a district border. Return the minimal number of district borders he must cross to get to his destination.*/
+
+//pseudosolution 111/124 testcases
+int crosses = 0;
+for(int k=0;k<x.length;k++) {
+  if(isInside(x1,y1,x[k],y[k],r[k]) && 
+     !isInside(x2,y2,x[k],y[k],r[k]) {
+        crosses ++;
+     }
+   if(isInside(x1,y1,x[k],y[k],r[k]) &&  !isInside(x2,y2,x[k],y[k],r[k]) {
+         return crosses
+   }
+     
+}
+//Helper method.
+ public boolean isInside(int x, int y, int cx, int cy, int r) {
+   double dist = (x-cx)*(x-cx) + (y-cy)*(y-cy);
+   double radSquared = r*r;
+   return dist < radSquared; //then true
+    
+ }
