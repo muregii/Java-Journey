fed55e6e10ef7340018c1391981e5343a6215aab
muregii <88530358+muregii@users.noreply.github.com>
2023-01-21 00:23:07 -0500

    Update CirclesCountry.java

--- Data Structures/APTS/CirclesCountry.java
+++ Data Structures/APTS/CirclesCountry.java
@@ -5,21 +5,27 @@ Imagine Circles Country as an infinite plane. You are given int[] x and int[] y 
 Neither of these points lies on a district border. Return the minimal number of district borders he must cross to get to his destination.*/
 
 //pseudosolution 111/228 testcases
+public class CirclesCountry {
+public int leastBorders (int[] x, int[] y, int[] r, int x1, int y1, int x2, int y2) {
 int crosses = 0;
+
 for(int k=0;k<x.length;k++) {
-  if(isInside(x1,y1,x[k],y[k],r[k]) && 
-     !isInside(x2,y2,x[k],y[k],r[k]) {
-        crosses ++;
+  if(isInside(x1,y1,x[k],y[k],r[k]) && ! isInside(x2,y2,x[k],y[k],r[k]) ) {
+        crosses +=1;
      }
-   if(isInside(x1,y1,x[k],y[k],r[k]) &&  isInside(x2,y2,x[k],y[k],r[k]) {
-         return crosses;
-   }
-     
+      else if(isInside(x2,y2,x[k],y[k],r[k]) ^  isInside(x1,y1,x[k],y[k],r[k])){
+          crosses ++;
+      }
+     }
+     return crosses;
 }
+
+
+
 //Helper method.
  public boolean isInside(int x, int y, int cx, int cy, int r) {
    double dist = (x-cx)*(x-cx) + (y-cy)*(y-cy);
    double radSquared = r*r;
    return dist < radSquared; //then true
-    
- }
+}
+}
