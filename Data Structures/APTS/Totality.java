688c3561a038f37d63929e3c5840eae31239d3fd
muregii <88530358+muregii@users.noreply.github.com>
2023-01-21 01:32:58 -0500

    Update Totality.java

--- Data Structures/APTS/Totality.java
+++ Data Structures/APTS/Totality.java
@@ -1,22 +1,24 @@
 public class Totality {
     public int sum (int[] a, String stype) {
-        int value = 0;
-        switch (stype) {
-            case "odd":
-                for(int i = 1;i<a.length;i+=2) {
-                    value += a[i];
-                }
-            case "even":
-                for (int i = 0; i<a.length;i+=2) {
-                    value += a[i];
-                }
-            default:
-            for (int i : a) {
-                value += i;
-            }
+       int value = 0;
 
+       switch(stype){
+        case "even":
+        for(int i=0;i<a.length;i+=2) {
+            value +=a[i];    
         }
-       return value;
-
+        break;
+        case "odd":
+        for(int i=1;i<a.length;i+=2) {
+            value+=a[i];
+       }
+       break;
+       default:
+       for(int i: a){
+        value += i;
+       }
+       
     }
+    return value;
+}
 }
