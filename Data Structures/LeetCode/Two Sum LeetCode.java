15e7e91a47a4421c2b79ede278b92e29008086ef
muregii <88530358+muregii@users.noreply.github.com>
2023-02-19 18:10:18 -0500

    Update Two Sum LeetCode.java

--- Data Structures/LeetCode/Two Sum LeetCode.java
+++ Data Structures/LeetCode/Two Sum LeetCode.java
@@ -1,16 +1,17 @@
 /* Give an array of integers [nums] and an integer [target], return indices of the two numbers such that they add up to the target*/
 
 class Solution {
-  public int[] twosum( int[] nums, int target) {
-    Map<Integer, Integer>numToIndex = new Hashmap<>();
-    
-   for(int i<0; i<nums.length[i];i++){
-     if(numToIndex.containsKey(target - nums[i]){
-       return new int[] { numToIndex.get(target - nums[i]), i}
-        }
-        numToIndex.put(nums[i], i);
-     throws new IllegalArgumentException();
-}
-}
-                                         
+    public int[] twoSum(int[] nums, int target) {
+      // two sum quiz
+    Map<Integer, Integer> numToIndex = new HashMap<>();
+
+    for (int i = 0; i < nums.length; ++i) {
+      if (numToIndex.containsKey(target - nums[i]))
+        return new int[] {numToIndex.get(target - nums[i]), i};
+      numToIndex.put(nums[i], i);
+    }
+
+    throw new IllegalArgumentException();
+  }
+}                                   
        
