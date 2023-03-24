cb058e2c44a76ac938c79a9e4b73b2dd0b7c7b4d
Raydon <88530358+muregii@users.noreply.github.com>
2023-03-24 07:44:38 -0400

    Update CirclesCountry.java

--- Data Structures/APTS/CirclesCountry.java
+++ Data Structures/APTS/CirclesCountry.java
@@ -4,7 +4,7 @@ he always tries to cross the fewest number of district borders as possible becau
 Imagine Circles Country as an infinite plane. You are given int[] x and int[] y and int[] r, where (x[i],y[i]) are the coordinates of the i-th district's center and r[i] is its radius. Qatam is currently at point (x1,y1) and he needs to get to point (x2,y2). 
 Neither of these points lies on a district border. Return the minimal number of district borders he must cross to get to his destination.*/
 
-//pseudosolution 111/228 testcases
+//check video explanation of this code @ https://youtu.be/9P63vhkgEoU
 public class CirclesCountry {
 public int leastBorders (int[] x, int[] y, int[] r, int x1, int y1, int x2, int y2) {
 int crosses = 0;
